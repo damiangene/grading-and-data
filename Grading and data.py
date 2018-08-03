7356fb3ac99e5ccb67c10583c2cb4f47a7b14cd6
Damiangene <damiangene1992@gmail.com>
2018-08-03 12:21:27 +0200

    average func added, user mark input changed to int(), print layout changed

--- Grading and data.py
+++ Grading and data.py
@@ -7,12 +7,25 @@ def action_choice():
     answer = str(input("Would you like to add marks or view the class marks? add/view/exit \n")).lower()
   
     if  answer == "view":
-      print (students) 
+      with open("names_marks.txt") as tmpfile:
+        students = json.load(tmpfile)
+      
+      for name in students: 
+        print (name)
+        print ("Average %.2f" % (mark_average(name)))
+        print(students[name])
+               
       exit()
 
     elif answer == "add": 
       student = str(input("Which student would you like to add to?"))
       return student
+
+    elif answer == "clear":
+        students = {}
+
+        with open('names_marks.txt', 'w') as outfile: 
+            json.dump(students, outfile, indent = 2)
 
     elif answer == "exit":
       exit()
@@ -24,6 +37,7 @@ def action_choice():
 def append_student():
   name = action_choice()
   mark = input("what mark did %s get?\n" % (name))
+  mark = int(mark)
   
   with open("names_marks.txt") as tmpfile:
         students = json.load(tmpfile)
@@ -37,5 +51,13 @@ def append_student():
         print (students)
         with open('names_marks.txt', 'w') as outfile: 
             json.dump(students, outfile, indent = 2)
+
+def mark_average(name):
+    with open("names_marks.txt") as tmpfile:
+        students = json.load(tmpfile)
+
+    total = sum(students[name]["Marks"])
+    s_average = total / len(students[name]["Marks"])
+    return s_average
 
 append_student()
